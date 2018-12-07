d74486b21701610445d9835905b36b6ecfa3fc63
Hoang-Nam <hnam.nguyen1708@gmail.com>
2018-12-07 08:37:27 +0100

    Get image data

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# coding=utf-8
 from PIL import Image, ImageFilter
+import numpy as np
 import matplotlib.pyplot as plt
 
 initialX = 70
@@ -6,29 +8,51 @@ initialY = 50
 deltaX = 170
 deltaY = 150
 
+
 #axis = 0
 #newXstart = previousX + 120
 #newDeltaX = previousXDelta + 120
 
 
 def main():
-    print("Hello World")
+    print("Debut Traitement.")
     im = Image.open("non_contrarie.bmp")
-    #im.show()
-    print(im.size)
-    box = (70, 50, 170, 150)
-    region = im.crop(box)
-    region = region.convert('L')
-    print(region.size)
-    region.show()
+    im.show()
+    imgWidth, imgHeight = im.size
+    print(imgWidth)
+    print(imgHeight)
 
-    nextRightBox = (190, 50, 290, 150)
-    nextRightRegion = im.crop(nextRightBox)
-    nextRightRegion.show()
+    #Variable Initialisation
+    xbegin = 70
+    ybegin = 170
+    widthBox = 50
+    heightBox = 150
+    rowMax = 3
+    colMax = 3
+    dataArray = [[0 for x in range(rowMax)] for y in range(colMax)]
 
-    secondNextBox = (310, 50, 410, 150)
-    secondNextRegion = im.crop(secondNextBox)
-    secondNextRegion.show()
+
+
+    # Parcours de la feuille
+    indexRow = 0
+    indexCol = 0
+    while indexCol < colMax:
+        while indexRow < rowMax:
+            box = (xbegin, widthBox, ybegin, heightBox)
+            region = im.crop(box)
+            region = region.convert('L')
+            dataArray[indexRow][indexCol] = region
+            #region.show()
+            xbegin += 120
+            ybegin += 120
+            indexRow += 1
+        indexCol += 1
+        indexRow = 0
+        widthBox += 120
+        heightBox += 120
+
+
+    dataArray[0][2].show()
 
     #plt.figure()
     #plt.imshow(region)
